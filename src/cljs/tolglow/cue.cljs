f3a89b06fd34c49c97b2014768ff41151cfa0eca
Joen Tolgraven <joen.tolgraven@gmail.com>
2019-07-23 15:07:16 +0200

    new way cue-[grid->page->row->cell]

--- src/cljs/tolglow/cue.cljs
+++ src/cljs/tolglow/cue.cljs
@@ -52,18 +52,15 @@
    @id)))
 (def new-cue-id (cue-id-gen))
 
-(defn create-blank-cue [x y]
- {:id (keyword (str (new-cue-id))) :color :gray :text "" :position [x y] :fx nil})
+(defn create-blank-cue [x y] ;or can we do :id :none?
+ {:id (keyword (str (new-cue-id))) :color "#575757" :text "" :position [x y] :fx nil})
 
 (rf/reg-sub :cue-at
  (fn [db [x y]]
    (let [cue (filter #(= [x y] (:position %)) (:cues db))]
     (if (some? cue) cue (create-blank-cue x y))))) ;not sure what's wrong with this...
 
-;shortcuts:
-;;   (html [:div#foo.bar.baz "bang"])
-;;   ->
-;;   "<div id=\"foo\" class=\"bar baz\">bang</div>"
+(def page (r/atom [0 0]))
 
 ;;   [:div>p>b "Nested Element"]
 ;;   ->
@@ -71,144 +68,97 @@
 ;;     [:p
 ;;       [:b "Nested Element"]]]
 
-(defn cue-cell [cue-data]
+(defn cue-cell [{:keys [color id] :as cue-data}]
  (let [s (r/atom cue-data)
-       a? #(:active @s)
-       toggle (fn [k & force] (swap! s update k #(if (seq force) (first force) (not %))))]
+       toggle (fn [k & force?] (swap! s update k #(or (first force?) (not %))))]
   (fn []
-   (let [
-         {:keys [color id whatever]} cue-data ;or do we create it blank and then sub to get from db I guess
-         ;; mm yup cause color can change tons etc... and obviously cues can be added and deleted while empty cue-cell keeps existing in some state
-          ;does that mean server pushes all colors at potentially 40fps tho sounds bit crazy
-         ]
+    [:td
+     {:style
+      {:box-shadow (str "1px 1px 6px 3px #345") ;how alpha?
+       ;; :outline (str "solid 4px " color) ;; :outline (str "solid 4px #282828" )
+       :border (str "1px solid #111") ;cant use dyn color it seems...
+       :color :white, :background-color #_color (if (:active @s) "#434a51" color) ;(cond->        #_color (@s :hovered)  #_(-> % (brighten 15%) (saturate 10%)) (@s :active)   #_(-> % (lighten 25%)) ;etc, other states: would kill other cue (way [too] faded in curr AG), maybe like "about to be included in new macro", a? color)
+       :width (* 60 (if (:hovered @s) 0.95 1.0)), :max-width 100 ;in AG max 100 but resizing cells jump from 90 to 116?? and max 142. no zoom
+       :height (* 50 (if (:active @s) 1.15 1.0)), :max-height 70
+       :transition "0.8s"
+       :font-size "60%"
+       :visibility (if (:fx @s) :visible :hidden)
+       :text-align "center" :word-wrap :break-word, :overflow :hidden
+       :display :table-cell ;:flex no  ;:grid no table-cell, etc. w3schools.com/cssref/pr_class_display.asp :display :inline-block ;everything ends up in same :td with this...
+       :margin-left 5, :margin-top 5, :padding 5} ;; :position :relative
+      :on-mouse-over (fn [e] (toggle :hovered true))
+      :on-mouse-out  (fn [e] (toggle :hovered false))
+      :on-click      (fn [e] (toggle :active) ;or we just dispatch and then sub or will everything get redrawn too much then?
+                      (rf/dispatch [:cue-clicked id e]))} ;"handle right click, shift click, anything else?")}
+     (:text cue-data) #_(:id cue-data)])))
 
-[:td
- {:style {;:background-color color ;"#86c"
-          ;; :box-shadow (str "2px 3px 8px 1px " color)
-          :box-shadow (str "2px 2px 7px 1px #678") ;how alpha?
-          :outline (str "solid 3px " color)
-          ;; :outline (str "solid 4px #282828" )
-          :border (str "2px solid #111") ;cant use dyn color it seems...
-          :bordercolor color ;"#ddd" #_(if a? "#484848" c)
-          :color :white
-          :background-color #_color (if (:active @s) "#434a51" color) #_(cond->        #_color
-                                                                 (@s :hovered)  #_(-> % (brighten 15%) (saturate 10%))
-                                                                 (@s :active)   #_(-> % (lighten 25%)) ;etc, other states: would kill other cue (way [too] faded in curr AG), maybe like "about to be included in new macro",
-                                                                 a? color) ;
-          :width (* 60 (if (:hovered @s) 0.95 1.0)), :max-width 100 ;in AG max 100 but resizing cells jump from 90 to 116?? and max 142. no zoom
-          :height (* 50 (if (:active @s) 1.15 1.0)), :max-height 70
-          :transition "0.8s" ;"height 0.5s width 1.5s" ;how set color transition and stuff?
-          :font-size "60%"
-          :text-align "center" :word-wrap :break-word :overflow :hidden
-          ;; :display :inline-block ;everything ends up in same :td with this...
-          :display :table-cell ;:flex no  ;:grid no table-cell, etc. w3schools.com/cssref/pr_class_display.asp
-          ;; :position :relative
-          :margin-left 5, :margin-top 5, :padding 5
-          ;; :top 0 :left 0 :right 0 :bottom 0 ;or I guess grid keeps track of poses
-          }
-  :on-mouse-over (fn [e] (toggle :hovered true))
-  :on-mouse-out (fn [e] (toggle :hovered false))
-  :on-click (fn [e]
-             (toggle :active) ;or we just dispatch and then sub or will everything get redrawn too much then?
-             (rf/dispatch [:cue-clicked id e])
-             (comment "handle right click, shift click, anything else?"))}
+(defn cue-row [cues & {:keys [wrap-tag cell-tag style]}] ;doesn't need to know location of self, just draws...
+ (fn [] ; :tbody, or :thead for top, or notthing?. any case looks like individual wraps work as well as whole table...
+  (let [row [:tr {:style style}
+             (doall
+              (for [cue cues]
+               (if cell-tag ;manual cell...
+                [cell-tag cue]
+                ^{:key (:id cue)}[cue-cell cue])))] ]
+   (if wrap-tag
+    [wrap-tag row]
+    row))))
 
- (:text cue-data)] #_(:id cue-data) ))))
-
-(def xy (r/atom (for [x (range 8) y (range 8)]
-                 @(rf/subscribe [:cue-at x y]))))
-
-(defn cue-page [pos-x pos-y] ;where is page located on larger grid?
- (let [s (r/atom {})]
-  (fn [] ;oh
-   (let [cues @(rf/subscribe [:cues #_show-key])
-         active? #(get-in @s [(:id %1) :active])
-         cues-padded @(rf/subscribe [:cues 8 8])
-         indiviual-subs (doall (for [x (range 8) y (range 8)]
-                                @(rf/subscribe [:cue-at x y])))]
-
-    [:table {:style {:width "100%" :text-align "left" ;:position :absolute
-                     :border "2px solid #789" :background-color "#333"
-                     :visible (:active @s false)
-                     :overflow :hidden}}
-      [:thead ;row above table proper, probably should be in cue-grid?
-       [:tr (for [i (range 8)] ;
-             ^{:key (str "cue-header" i)}
-             [:th
-              {:style {:text-align "center"}}
-              (inc i)])]]
-
-       (let [dummies (vec (doall (for [y (range 8) x (range 8)] ; y/x since we do by row
-                      (create-blank-cue x y)))) ;wanna go this approach since need to be able to handle on the fly creation and destruction anyways...
-             idx-for-xy (fn [[x y]] (+ x (* 8 y))) ;]
-             page (reduce
-                    (fn [grid cue] (assoc grid (idx-for-xy (:position cue)) cue))
-                    dummies cues)]
-
-        ;; (reduce ;try do below nicer
-        ;;  (fn [[row table] cues]
-        ;;   (let [])))
-      (loop [cues #_indiviual-subs page row [:tr], table []]
-       (let [row (into row
-                       (doall
-                        (map-indexed ;; [:td ^{:key (:id cue)}[cue-cell cue]]
-                         (fn [i cue]
-                          ^{:key (:id cue)}[cue-cell cue])
-                         (take 8 cues))))]
-       (if (< 8 (count cues))
-        (recur (drop 8 cues), [:tr], (concat table [row]))
-        (concat table [row])))))
-
-
-       [:tbody ;row below table proper
-        [:tr (for [i (range 8)]
-             ^{:key (str "cue-footer" i)}
-             [:td
-              {:style {:text-align "center"}}
-              (inc i)])]]
-       ]))))
-
-;; (defn nav-btn "Button to change page" [x-offset y-offset]
-;;  (let [thing (rf/)]
-;;   (fn []
-;;   [:div
-;;    [:lala-presentation]
-;;    {:style {}
-;;     :on-click #(rf/dispatch [:view-change x-offset y-offset])}
-;;    ])))
+(defn cue-page [page-xy] ;where is page located on larger grid?
+ (let [[cell-x cell-y] (map #(* 8 %) page-xy)
+       cues @(rf/subscribe [:cues]) ;should be limited to those on curr page
+       dummies (vec (doall (for [y (range 8) x (range 8)] ; y/x since we do by row
+                            (create-blank-cue x y)))) ;wanna go this approach since need to be able to handle on the fly creation and destruction anyways...
+       idx-for-xy (fn [[x y]] (+ x (* 8 y))) ;this is to find index in monovector but should prob group em up earlier yet or?
+       rows (->> (reduce
+                  (fn [grid cue]
+                   (assoc grid (idx-for-xy (:position cue)) cue))
+                  dummies cues)
+                 (partition 8)
+                 reverse)]
+  (fn [] [:tbody (doall (for [row rows] [cue-row row]))])))
 
 (defn cue-grid "Viewcontroller for [cue-page]" [page-data]
- (let [s (r/atom {:view [0 0]})] ;so do we only have one grid and swap in and out cue-rects to simulate changing
+ (let [s (r/atom {:view [0 0]})
+       view (rf/subscribe [:view])] ;so do we only have one grid and swap in and out cue-rects to simulate changing
   ; or do we have many instances of cue-page all ready to go? wanting other views than 8x8 sounds possible so making it super dynamic (if works performance-wise) soudns good
   ; also simplifies a bit if what is dispayed is what "exists", no throttling state updates for stuff out of view or whatever - it doesnt concern us until it does
   (fn []
-   [:table {:style {:width "100%" :text-align "left" ;:position :absolute
-                    :border "2px solid #789" :background-color "#333"
-                    :visible (:active @s false)
-                    :overflow :hidden}}
-      [:thead ;row above table proper, probably should be in cue-grid?
-       [:tr (for [i (range 8)] ;
-             ^{:key (str "cue-header" i)}
-             [:th
-              {:style {:text-align "center"}}
-              (inc i)])]]
+   (let [#_(:view @s)]
+    [:table.cue-grid
+     {:style {:width "100%" :text-align "center" ;"left" ;why do we need that?
+              :border "2px solid #789" :background-color #_"hsl(20, 60%, 50%)" "#333"
+              :overflow :hidden}}
+     [cue-row (range 1 9) :wrap-tag :thead :cell-tag :th ;top
+      :style {:text-align "center"}] ;align no work. also the unique ID thing. send along a more exact cell spec?
 
-       [cue-page 0 0]
+     [cue-page @page]
 
-       [:tbody ;row below table proper
-        [:tr (for [i (range 8)]
-             ^{:key (str "cue-footer" i)}
-             [:td
-              {:style {:text-align "center"}}
-              (inc i)])]]
+     [cue-row (range 1 9) :wrap-tag :tbody :cell-tag :td ;bottom
+      :style {:text-align "center"}] ]) )))
 
-       ;; [nav-btn :left-glyph #(swap! s #(update ))] ;still cooler to have one arrow and set rotation by move offset vector :P
 
-       ;; [nav-btn 1  0 #(swap! s #(update s :view #(map + %)))]
-       ;; [nav-btn 0  1]
-       ;; [nav-btn -1 0]
-       ;; [nav-btn 0 -1]
-       ])))
+(defn nav-btn "Single button"
+ [x-move y-move s]
+ [:td
+  {:style {:text-align :center :height 60 :width 80 :padding 10
+           :border "3px solid #789" :display :table-cell}
+   :on-click (fn []
+              (swap! page (fn [pos]
+                           (map #(if (<= 0 (+ %1 %2)) (+ %1 %2) 0)
+                                pos [x-move y-move])))
+              (rf/dispatch [:view-change @page]))}
+  (str x-move " " y-move)])  ;for now. fix a nice arrow and rotate somehow
+
+(defn nav-btns "Buttons to change page" [s]
+  (fn []
+  [:div
+    [:table.nav-btns {:style {:background-color "#282828" } }
+    [:tbody
+     (for [y (reverse (range -1 2))]
+      [:tr (for [x (range -1 2)]
+        [nav-btn x y s])])]]])) ; :arrow-left :arrow-right :arrow-bottom :arrow-left-bottom :arrow-right-top :long-arrow-up :long-arrow-down
+; uhh rest? also what about just rotating one?
 
 
 (defn header-bar []
@@ -223,8 +173,11 @@
 
 (defn ui []
   [:div
-   [header-bar]
-   [cue-page]])
+   ;; [header-bar]
+   [cue-grid]
+   [:p]
+   [nav-btns page]
+   #_[test-re-con]])  ;could even send s along and have it swapped from there?
 
 (when-some [el (js/document.getElementById "cue-db")]
   (defonce _init (rf/dispatch-sync [:initialize]))
