7bb05ca6e22dbccbfea9e37abf3e61f5a082aaa1
Joen Tolgraven <joen.tolgraven@gmail.com>
2019-07-30 05:56:32 +0200

    move sample data and db init to db.cljs

--- src/cljs/tolglow/cue.cljs
+++ src/cljs/tolglow/cue.cljs
@@ -1,4 +1,4 @@
-(ns tolglow.cue ;fix hehe
+(ns tolglow.cue
   (:require [reagent.core :as r]
             [recalcitrant.core :refer [error-boundary]]
             [re-com.core :as rc ]
@@ -6,32 +6,7 @@
             [tolglow.db :as db]))
 
 
-(defn safe "Wrap whatever with an error boundary"
- [elem] (r/as-element [(fn [] [error-boundary elem])]))
-
-(def data
- {:cues
- [{:id :1 :color :blue :text "Run shite" :position [0 0] :fx "Strobe"}
-  {:id :3 :color :red :text "Extra sparkle" :position [1 0] :fx "Color"}
-  {:id :11 :color :green :text "Moving" :position [2 0] :fx "Pan"}
-  {:id :10 :color :seagreen :text "Moving" :position [3 0] :fx "Aim"}
-  {:id :0 :color :purple :text "Fog" :position [4 0] :fx "Fsshh!"}
-  {:id :200 :color :orange :text "Stripe" :position [0 1] :fx "Tilt"}
-  {:id :19 :color :black :text "Wash" :position [1 1] :fx "Shutter"}
-  {:id :100 :color :orangered :text "Pointy" :position [2 1] :fx "Blame"}
-
-  {:id :108 :color :seagreen :text "2nd ROW" :position [6 0] :fx "YO"}
-  {:id :109 :color :purple :text "2nd ROW"   :position [6 1] :fx "YO"}
-  {:id :110 :color :seagreen :text "2nd ROW" :position [6 2] :fx "YO"}
-  {:id :111 :color :seagreen :text "2nd ROW" :position [6 3] :fx "YO"}
-  {:id :112 :color :seagreen :text "2nd ROW" :position [6 4] :fx "YO"}
-  {:id :113 :color :seagreen :text "2nd ROW" :position [6 5] :fx "YO"}
-  ]})
-
-(rf/reg-event-db :initialize
- (fn [db _] {:cues (:cues data)}))
-
-(rf/reg-event-db :create-cue
+(rf/reg-event-db :create-cue ;this needs to like send a :cues event as well no? for earlier subs to react...
  (fn [db [_ c]] (update db :cues #(conj c %))))
 (rf/reg-event-db :delete-cue
  (fn [db [_ c]] (update db :cues #(disj % c)))) ;can just delete from list and will be replaced by a dummy. tho bit inefficient to trigger entire process from one dunno
