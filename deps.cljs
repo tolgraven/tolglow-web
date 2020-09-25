6c7477a2fb1242a945dc6713d002c6246aac4eb5
Joen Tolgraven <joen.tolgraven@gmail.com>
2020-09-25 06:54:12 +0200

    what I didnt have env or checkouts in repo?

---
+++ deps.cljs
@@ -0,0 +1,12 @@
+{:foreign-libs
+ [{:file "cljsjs/react-color/development/react-color.inc.js",
+   :provides ["cljsjs.react-color"],
+   :requires
+   ["cljsjs.react"
+    "cljsjs.lodash"
+    "cljsjs.tinycolor"
+    "cljsjs.proptypes"
+    "cljsjs.material-colors"
+    "cljsjs.reactcss"],
+   :file-min "cljsjs/react-color/production/react-color.min.inc.js"}],
+ :externs ["cljsjs/react-color/common/react-color.ext.js"]}
